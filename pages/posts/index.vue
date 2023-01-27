37cdf2f764412fc5d251f88e30e0dc8eb3c3111d
TNT <a.esmaeili.sut@gmail.com>
2023-01-27 14:46:54 +0330

    add infinte scroll and show posts

---
+++ pages/posts/index.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useInfiniteQuery, useQuery } from '@tanstack/vue-query'
+import { useDebounceFn, useIntersectionObserver } from '@vueuse/core'
+
+const { $client } = useNuxtApp()
+
+const PER_PAGE_LENGTH = 10
+
+const spinnerRef = ref<HTMLElement | null>(null)
+const searchText = ref('')
+const isNextPageAvailable = ref<boolean>(true)
+
+const updateSearchText = useDebounceFn((newValue: string): void => {
+  searchText.value = newValue
+}, 700)
+
+const clearSearchText = () => {
+  searchText.value = ''
+}
+
+useIntersectionObserver(spinnerRef, ([{ isIntersecting }]): void => {
+  if (isIntersecting) {
+    // API is so fast! For visual purpose i use setTimeOut to display spinner
+    setTimeout(() => {
+      fetchNextPage()
+    }, 1000)
+  }
+})
+const {
+  data: posts,
+  isLoading,
+  isError,
+  error,
+  fetchNextPage
+} = useInfiniteQuery({
+  queryKey: ['postsList', searchText],
+  queryFn: () => $client.posts.query({ limit: 5 }),
+  staleTime: 0,
+  getNextPageParam: (lastPage) => {
+    return lastPage.nextCursor
+  }
+})
+
+</script>
+
+<template>
+  <span v-if="isLoading">Loading...</span>
+  <span v-else-if="isError">Error: {{ error.message }}</span>
+  <!-- We can assume by this point that `isSuccess === true` -->
+  <table v-else border="">
+    <thead>
+      <tr>
+        <th> Id </th>
+        <th> Title </th>
+        <th> Content </th>
+        <th> Last Update </th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="(page, ind) in posts?.pages" :key="ind">
+        <tr v-for="post in page.items" :key="post.id">
+          <td>{{ post.id }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.content }}</td>
+          <td>{{ post.updatedAt }}</td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+</template>
--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod'
+import { Prisma } from '@prisma/client'
 import { publicProcedure, router } from '../trpc'
 
+const defaultPostSelect = Prisma.validator<Prisma.PostSelect>()({
+  id: true,
+  title: true,
+  content: true,
+  updatedAt: true
+})
 export const appRouter = router({
   hello: publicProcedure
     .input(
@@ -13,7 +20,45 @@ export const appRouter = router({
         greeting: `hello ${input?.text ?? 'world'}`,
         time: new Date()
       }
+    }),
+  posts: publicProcedure.input(
+    z.object({
+      limit: z.number().min(1).max(100).nullish(),
+      cursor: z.string().nullish()
     })
+  ).query(async ({ input, ctx }) => {
+    const limit = input.limit ?? 50
+    const { cursor } = input
+
+    const posts = await ctx.prisma.post.findMany({
+      select: defaultPostSelect,
+      // get an extra item at the end which we'll use as next cursor
+      take: limit + 1,
+      where: {},
+      cursor: cursor
+        ? {
+            id: cursor
+          }
+        : undefined,
+      orderBy: {
+        createdAt: 'desc'
+      }
+
+    })
+    let nextCursor: typeof cursor | undefined
+    if (posts.length > limit) {
+      // Remove the last item and use it as next cursor
+
+      const nextItem = posts.pop()!
+      nextCursor = nextItem.id
+    }
+    return {
+      items: posts.reverse(),
+      nextCursor
+    }
+  }
+
+  )
 })
 
 // export type definition of API
